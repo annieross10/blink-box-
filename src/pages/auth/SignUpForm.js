afe39bcf92a0ba68a85b6d604227151b69577e8a
annieross10 <annieross10@outlookcom>
2024-03-13 16:46:03 +1000

    create signupform.js

---
+++ src/pages/auth/SignUpForm.js
@@ -0,0 +1,93 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Container, Row, Col } from "react-bootstrap";
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <Row className="mt-5">
+      <Col md={{ span: 6, offset: 3 }}>
+        <Container>
+          <h1 className="text-center">Sign Up</h1>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicUsername">
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter username"
+                name="username"
+                value={formData.username}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Enter email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicConfirmPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Confirm Password"
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+            </Form.Group>
+
+            <Button variant="primary" type="submit" block>
+              Sign Up
+            </Button>
+          </Form>
+          <p className="mt-3 text-center">
+            Already have an account? <Link to="/signin">Sign in</Link>
+          </p>
+        </Container>
+      </Col>
+    </Row>
+  );
+};
+
+export default SignUpForm;
