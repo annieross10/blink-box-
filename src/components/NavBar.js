6504643896b5a4ae2598e1800e6b8393cceb1701
annieross10 <annieross10@outlookcom>
2024-03-13 16:11:53 +1000

    upload favicon icon and NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,37 @@
 import React from "react";
 import { Navbar, Container, Nav, Button } from "react-bootstrap";
 import logo from '../assets/logo.png'
+import styles from '../styles/NavBar.module.css'
+import { NavLink} from "react-router-dom"
 
 const NavBar = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="md" fixed="top">
+    <Navbar className={styles.NavBar} bg="dark" variant="dark" expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="45"/>BLINKBOX</Navbar.Brand>
+    <NavLink to="/">
+        <Navbar.Brand href="#home"><img src={logo} alt="logo" height="50"/>BLINKBOX
+        </Navbar.Brand>
+    </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#login">Login</Nav.Link>
-            <Button variant="light" href="#signup">Sign Up</Button>
+            <NavLink 
+                exact
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/">Home</NavLink>
+            <NavLink
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/Login">Login</NavLink>
+            <NavLink 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to="/signup">
+                <Button variant="light" href="#signup" className={styles["sign-up-link"]}>
+                    Sign Up
+                </Button>
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
